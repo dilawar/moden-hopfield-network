919e5ad248d2d0e716d4a8efb9aab2e09987796f
Dilawar Singh <dilawar@subcom.tech>
2024-11-12 15:22:09 +0530

    Fix cli tests

--- tests/test_cli.rs
+++ tests/test_cli.rs
@@ -1,14 +1,14 @@
 //! test meena cli.
 
-use assert_cmd::prelude::*; // Add methods on commands
-use predicates::prelude::*; // Used for writing assertions
-use std::process::Command; // Run programs
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
 
-const MEENA_CLI_NAME: &str = "libmeena";
+const CLI_NAME: &str = "modern-hopfield-network";
 
 #[test]
 fn cli_help() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin(MEENA_CLI_NAME)?;
+    let mut cmd = Command::cargo_bin(CLI_NAME)?;
     cmd.arg("help");
     cmd.assert().success();
     Ok(())
@@ -17,28 +17,28 @@ fn cli_help() -> Result<(), Box<dyn std::error::Error>> {
 #[test]
 fn cli_xor() -> Result<(), Box<dyn std::error::Error>> {
     const DAM_NAME: &str = "xor";
-    let mut cmd = Command::cargo_bin(MEENA_CLI_NAME)?;
+    let mut cmd = Command::cargo_bin(CLI_NAME)?;
     cmd.args(["--name", DAM_NAME, "clear"]);
     cmd.assert().success();
 
-    let mut cmd = Command::cargo_bin(MEENA_CLI_NAME)?;
+    let mut cmd = Command::cargo_bin(CLI_NAME)?;
     cmd.args(["--name", DAM_NAME, "train", "0,0", "0", "2"]);
     cmd.assert().success();
 
-    cmd = Command::cargo_bin(MEENA_CLI_NAME)?;
+    cmd = Command::cargo_bin(CLI_NAME)?;
     cmd.args(["--name", DAM_NAME, "train", "0,1", "1", "2"]);
     cmd.assert().success();
 
-    cmd = Command::cargo_bin(MEENA_CLI_NAME)?;
+    cmd = Command::cargo_bin(CLI_NAME)?;
     cmd.args(["--name", DAM_NAME, "train", "1,0", "1", "2"]);
     cmd.assert().success();
 
-    cmd = Command::cargo_bin(MEENA_CLI_NAME)?;
+    cmd = Command::cargo_bin(CLI_NAME)?;
     cmd.args(["--name", DAM_NAME, "train", "1,1", "0", "2"]);
     cmd.assert().success();
 
     for (pat, res) in [("0,0", "0"), ("0,1", "1"), ("1,0", "1"), ("1,1", "0")] {
-        cmd = Command::cargo_bin(MEENA_CLI_NAME)?;
+        cmd = Command::cargo_bin(CLI_NAME)?;
         cmd.args(["--name", DAM_NAME, "classify", pat]);
         cmd.assert().success().stdout(predicate::str::contains(res));
     }
